904531863037541bcab51a6e1601c05ca3ed075d
anmol05032002 <144431059+anmol05032002@users.noreply.github.com>
2026-07-03 14:47:51 +0530

    Time: 0 ms (100.00%) | Memory: 25.5 MB (45.47%) - LeetSync

--- 530-minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.cpp
+++ 530-minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.cpp
@@ -11,25 +11,40 @@
  */
 class Solution {
 public:
-  void inorder(TreeNode* root, vector<int> &temp){
-    if(root==NULL)return;
+//   void inorder(TreeNode* root, vector<int> &temp){
+//     if(root==NULL)return;
     
-    inorder(root->left,temp);
-    temp.push_back(root->val);
-    inorder(root->right,temp);
-  }
+//     inorder(root->left,temp);
+//     temp.push_back(root->val);
+//     inorder(root->right,temp);
+//   }
+int diff_ans = INT_MAX;
+TreeNode* prev = NULL;
+void inorder(TreeNode* root){
+    if(root==NULL)return;
+
+    inorder(root->left);
+    if(prev!=NULL){
+        diff_ans = min(diff_ans,abs(prev->val - root->val));  
+    }
+    prev = root;
+    inorder(root->right);
+}
     int getMinimumDifference(TreeNode* root) {
-       vector<int> temp;
-       inorder(root,temp);
+    //    vector<int> temp;
+    //    inorder(root,temp);
 
-       int n = temp.size();
+    //    int n = temp.size();
 
-       int diff = INT_MAX;
+    //    int diff = INT_MAX;
 
-       for(int i =0; i<n-1; i++){
-        int x = abs(temp[i]-temp[i+1]);
-        diff = min(diff,x);
-       }
-       return diff;
+    //    for(int i =0; i<n-1; i++){
+    //     int x = abs(temp[i]-temp[i+1]);
+    //     diff = min(diff,x);
+    //    }
+    //    return diff;
+
+    inorder(root);
+    return diff_ans;
     }
 };
