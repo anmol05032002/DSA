5b656ff336e0ce61d50c7738fcd3cec0bc1f2474
anmol05032002 <144431059+anmol05032002@users.noreply.github.com>
2026-07-03 12:46:48 +0530

    Time: 0 ms (100.00%) | Memory: 65 MB (59.98%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.cpp
+++ 975-range-sum-of-bst/range-sum-of-bst.cpp
@@ -23,8 +23,20 @@ public:
 
    }
     int rangeSumBST(TreeNode* root, int low, int high) {
-        int sum =0;
-        solve(root,low,high,sum);
-        return sum;
+        // int sum =0;
+        // solve(root,low,high,sum);
+        // return sum;
+
+        if(root==NULL)return 0;
+         if(root->val>=low && root->val<=high){
+            return root->val + rangeSumBST(root->left,low,high) + rangeSumBST(root->right,low,high);
+        }
+        if(root->val<low){
+            return rangeSumBST(root->right,low,high);
+        }
+      
+          return  rangeSumBST(root->left,low,high);
+        
+        
     }
 };
