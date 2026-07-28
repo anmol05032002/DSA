1fc6fe0a6c3b513282365bd5d8e994a12fa50816
anmol05032002 <144431059+anmol05032002@users.noreply.github.com>
2026-07-28 16:39:34 +0530

    Time: 116 ms (22.86%) | Memory: 75 MB (38.24%) - LeetSync

---
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+
+  void reverse(string &s){
+    int n = s.length();
+
+    int i =0;
+    int j = n-1;
+
+    while(i<j){
+        swap(s[i],s[j]);
+        i++;
+        j--;
+    }
+  }
+    string smallestPalindrome(string s) {
+        string first_half = "";
+        int n = s.length();
+
+        if(n==1)return s;
+
+        int mid = n/2;
+
+        char middle = s[mid];
+
+        for(int i =0; i<n/2; i++){
+            first_half.push_back(s[i]);
+        }
+
+        sort(first_half.begin(),first_half.end());
+        // return first_half + middle;
+
+        string rev = first_half;
+
+        reverse(rev);
+
+        string ans = "";
+
+        if(n%2)
+       ans =  first_half + middle + rev;
+
+       else
+       ans = first_half + rev;
+
+
+       return ans;
+
+
+       return ans;
+
+
+    }
+};
