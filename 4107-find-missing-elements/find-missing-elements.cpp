468a41d06b38a791106fc1574a3a4ea4f9adf41c
anmol05032002 <144431059+anmol05032002@users.noreply.github.com>
2026-08-07 00:20:18 +0530

    Time: 2 ms (57.39%) | Memory: 34.8 MB (8.05%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.cpp
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -3,18 +3,45 @@ public:
     vector<int> findMissingElements(vector<int>& nums) {
         int n = nums.size();
 
-        sort(nums.begin(),nums.end());
-        int smallest = nums[0];
+    //     sort(nums.begin(),nums.end());
+    //     int smallest = nums[0];
       vector<int> ans;
-        for(int i =0; i<n; i++){
-            if(nums[i]==(smallest + i)) continue;
-            else{
-                int x = nums[i-1];
-                int y = nums[i];
-                for(int j = x+1; j<y; j++)
-                ans.push_back(j);
-            }
+    //     for(int i =0; i<n; i++){
+    //         if(nums[i]==(smallest + i)) continue;
+    //         else{
+    //             int x = nums[i-1];
+    //             int y = nums[i];
+    //             for(int j = x+1; j<y; j++)
+    //             ans.push_back(j);
+    //         }
+    //     }
+
+
+    int maxi = INT_MIN;
+     for(int i=0; i<n; i++){
+        maxi = max(maxi,nums[i]);
+     }
+
+     int mini = INT_MAX;
+     for(int i=0; i<n; i++){
+        mini = min(mini,nums[i]);
+     }
+
+     unordered_map<int,bool>mp;
+
+     for(int i = 0; i<n; i++){
+        mp[nums[i]] = true;
+     }
+
+
+     for(int i = mini; i<=maxi; i++){
+        if(mp[i]){
+            continue;
         }
+        else{
+            ans.push_back(i);
+        }
+     }
         return ans;
     }
 };
