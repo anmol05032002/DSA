5b9f03fab01fc773d1d52cf7091e9212c60301fc
anmol05032002 <144431059+anmol05032002@users.noreply.github.com>
2026-08-14 14:14:37 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (44.69%) - LeetSync

---
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maximumLengthSubstring(string s) {
+        int n = s.length();
+
+        int i =0;
+        int j = 0;
+
+        unordered_map<char,int>mp;
+        int count =0;
+        while(j<n){ 
+           mp[s[j]]++;
+            while(mp[s[j]]>2){
+                mp[s[i]]--;
+                i++;
+            }
+           count = max(count,j-i+1);
+           j++;
+        }
+        return count;
+    }
+};
