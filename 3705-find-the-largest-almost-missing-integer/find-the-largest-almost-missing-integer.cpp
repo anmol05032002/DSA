2932db8478dff3d6557386dfd2f01c9c5aecb205
anmol05032002 <144431059+anmol05032002@users.noreply.github.com>
2026-08-18 14:30:08 +0530

    Time: 0 ms (100.00%) | Memory: 29.3 MB (56.02%) - LeetSync

--- 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.cpp
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.cpp
@@ -7,32 +7,33 @@ public:
         int i =0;
         int j = 0;
 
-        while(j<n){
-            if(j-i+1<=k)
-            {
-              mp[nums[j]]++;
-              j++;
-            }
-            else{
-                i++;
-                j = i;
-            }
+        // while(j<n){
+        //     if(j-i+1<=k)
+        //     {
+        //       mp[nums[j]]++;
+        //       j++;
+        //     }
+        //     else{
+        //         i++;
+        //         j = i;
+        //     }
 
-        }
+        // }
 
-        int maxi = -1;
+        // int maxi = -1;
         bool present = false;
 
-        for(auto it:mp){
-            int a = it.first;
-            int b = it.second;
-            if(b==1){
-                present = true;
-                maxi = max(maxi,a);
-            }
-        }
+        // for(auto it:mp){
+        //     int a = it.first;
+        //     int b = it.second;
+        //     if(b==1){
+        //         present = true;
+        //         maxi = max(maxi,a);
+        //     }
+        // }
 
         int maxm = -1;
+        int ans = -1;
 
         for(int i=0; i<n; i++){
             maxm = max(maxm,nums[i]);
@@ -40,8 +41,39 @@ public:
 
         if(k==n)return maxm;
 
-        else if(present)return maxi;
+        // else if(present)return maxi;
 
+       
+
+     while(i<n){
+            mp[nums[i]]++;
+            i++;
+        }
+         for(auto it:mp){
+            int a = it.first;
+            int b = it.second;
+                if(b==1){
+                    present = true;
+                    ans = max(ans,a);
+                }
+        }
+       
+       if(k==1 && present)return ans;
+       
+       int first = mp[nums[0]];
+       int last = mp[nums[n-1]];
+
+       if(k!=1 && k!=n){
+           if(first!=1 && last ==1){
+            return nums[n-1];
+           }
+           else if(first==1 && last!=1){
+            return nums[0];
+           }
+           else if(first==1 && last==1){
+            return max(nums[0],nums[n-1]);
+           }
+       }
         return -1;
     }
 };
