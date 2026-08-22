11143e0ca7cb6b35f897bf8d3a110b7c11b85d2c
anmol05032002 <144431059+anmol05032002@users.noreply.github.com>
2026-08-22 18:49:39 +0530

    Time: 1 ms (92.53%) | Memory: 127.6 MB (87.26%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<int> ans;
-        int n  = nums.size();
-
-        for(int i=0; i<n; i++){
-            if(nums[i]<pivot){
-                ans.push_back(nums[i]);
+        vector<int> result(nums.size(), 0);
+        int left = 0, right = nums.size() - 1;
+        
+        for (int i = 0, j = nums.size() - 1; i < nums.size(); ++i, --j) {
+            if (nums[i] < pivot) {
+                result[left] = nums[i];
+                left++;
+            }
+            
+            if (nums[j] > pivot) {
+                result[right] = nums[j];
+                right--;
             }
         }
-         for(int i=0; i<n; i++){
-            if(nums[i]==pivot){
-                ans.push_back(nums[i]);
-            }
+        
+        while (left <= right) {
+            result[left] = pivot;
+            left++;
         }
-         for(int i=0; i<n; i++){
-            if(nums[i]>pivot){
-                ans.push_back(nums[i]);
-            }
-        }
-    return ans;
-
+        
+        return result;
     }
 };
