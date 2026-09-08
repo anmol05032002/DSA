735527140dc3826270101a2e0d99662be66be866
anmol05032002 <144431059+anmol05032002@users.noreply.github.com>
2026-09-08 17:07:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (84.49%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.cpp
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -4,13 +4,15 @@ public:
         // return 3;
         int x = n;
 
-        int count_digt = 0;
-        while(x!=0){
-            x/=10;
-            count_digt++;
-        }
+        // int count_digt = 0;
+        // while(x!=0){
+        //     x/=10;
+        //     count_digt++;
+        // }
 
-        if(count_digt>3)return count_digt/4 + n-1000;
+        // if(count_digt>3)return count_digt/4 + n-1000;
+
+        if(n>999)return n-999;
         return 0;
     }
 };
