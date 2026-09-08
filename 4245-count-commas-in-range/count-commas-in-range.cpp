e20e4a8b4fbf99ad19b82426072f8ec5c4d5cddc
anmol05032002 <144431059+anmol05032002@users.noreply.github.com>
2026-09-08 10:46:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (84.49%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        // return 3;
+        int x = n;
+
+        int count_digt = 0;
+        while(x!=0){
+            x/=10;
+            count_digt++;
+        }
+
+        if(count_digt>3)return count_digt/4 + n-1000;
+        return 0;
+    }
+};
